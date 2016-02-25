4f4fa4da4c646ea50598985950d1adf92514d119
Bo Yan <boven.yan@gmail.com>
2016-02-25 00:45:29 -0500

    commit file

---
+++ switching/ovs_prepare.sh
@@ -0,0 +1,6 @@
+/sbin/modprobe openvswitch
+ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
+             --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
+             --pidfile --detach
+ovs-vsctl --no-wait init
+ovs-vswitchd --pidfile --detach
