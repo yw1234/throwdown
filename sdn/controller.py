b97adfbeb083f82357b659743e4f1b2af80ab112
Bo Yan <boven.yan@gmail.com>
2016-02-25 12:35:42 -0500

    add failover and migration functions

--- sdn/controller.py
+++ sdn/controller.py
@@ -6,8 +6,11 @@ from ryu.ofproto import ofproto_v1_0
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import arp
+from ryu.lib.packet import ipv4, tcp, udp, icmp
 from ryu.lib.packet import ether_types
 import multiprocessing
+import randint
+import copy
 
 class SimpleSwitch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
@@ -24,33 +27,221 @@ class SimpleSwitch(app_manager.RyuApp):
 
         self.datapaths = {}
 
-        self.flowpath = []
-        flowpath.append(["192.168.3.1", "192.168.3.2"])
-        flowpath.append(["192.168.4.1", "192.168.4.2"])
+        self.path_info = []  # west, east, west/east port
+        self.path_info.append(["192.168.3.2", "192.168.3.1", 1])
+        self.path_info.append(["192.168.4.2", "192.168.4.1", 3])
+        self.vBundle_info = ["192.168.5.2", "192.168.5.1", 2]
 
-    def add_flow(self, datapath, match, actions):
+        self.lsp_rules = []  # recording the associated flow for each lsp
+        lsp_rules.append({})  # [cookie : [rule]]
+        lsp_rules.append({})  # [cookie : [rule]]
+
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        dpid = datapath.id
+
+        # default fwd everything to controller
+        actions = [datapath.ofproto_parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
+        # match = datapath.ofproto_parser.OFPMatch(dl_type=ether_types.ETH_TYPE_ARP)
+        match = datapath.ofproto_parser.OFPMatch()
+        self.add_flow(datapath, match, actions, 0)
+
+        if dpid == 82 or dpid == 81:    # record datapath
+            self.datapaths[dpid] = dapapath
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+        in_port = msg.in_port
+
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+
+        # handle arp
+        if eth.ethertype == ether_types.ETH_TYPE_ARP:
+            pkt_arp = pkt.get_protocol(arp.arp)
+            self._handle_arp(datapath, in_port, eth, pkt_arp)
+
+        # handle ip
+        if eth.ethertype == ether_types.ETH_TYPE_IP:
+            ip_pkt = pkt.get_protocol(ipv4.ipv4)
+
+            if (ip_pkt.proto == 6):  # tcp
+                tcp_pkt = pkt.get_protocol(tcp.tcp)
+                self.handle_ip(datapath.id, ip_pkt.proto, tcp_pkt.src_port,
+                               tcp_pkt.dst_port, 0)  ### select the first path
+
+            elif(ip_pkt.proto == 17):  # udp
+                udp_pkt = pkt.get_protocol(udp.udp)
+                self.handle_ip(datapath.id, ip_pkt.proto, udp_pkt.src_port,
+                               udp_pkt.dst_port, 0)
+
+            else: # icmp
+                self.handle_ip(datapath.id, ip_pkt.proto, None, None, 0)
+
+            self.packet_out(datapath, msg.data, in_port, self.vBundle_info[0])
+
+    def lsp_failover(orig_lsp, new_lsp):
+        for cookie in lsp_rules[orig_lsp]:
+            self.del_flow(cookie)
+
+            orig_rule = lsp_rules[orig_lsp][cookie]
+            self.handle_ip(orig_rule[0], orig_rule[1], orig_rule[2],
+                           orig_rule[3], new_lsp)
+
+        lsp_rules[orig_lsp].clear()
+
+    def flow_migration(cookie, orig_lsp, new_lsp):
+        self.del_flow(cookie)
+        orig_rule = lsp_rules[orig_lsp]
+        self.handle_ip(orig_rule[0], orig_rule[1], orig_rule[2],
+                       orig_rule[3], new_lsp)
+        del lsp_rules[cookie]
+
+    def _handle_arp(self, datapath, port, eth, pkt_arp):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        if pkt_arp.opcode != arp.ARP_REQUEST:
+            return
+        pkt = packet.Packet()
+
+        if pkt_arp.dst_ip in self.arptable:
+           fin_src_mac = self.arptable[pkt_arp.dst_ip]
+
+        pkt.add_protocol(ethernet.ethernet(ethertype=eth.ethertype, dst=eth.src,
+                                           src=fin_src_mac))
+        pkt.add_protocol(arp.arp(opcode=arp.ARP_REPLY,
+                                 src_mac=fin_src_mac, src_ip=pkt_arp.dst_ip,
+                                 dst_mac=eth.src, dst_ip=pkt_arp.src_ip))
+        pkt.serialize()
+        data = pkt.data
+        actions = [parser.OFPActionOutput(port=port)]
+        self.logger.info("packet out for arp reply %s" %(pkt,))
+
+        out = parser.OFPPacketOut( datapath=datapath,
+                                   buffer_id=ofproto.OFP_NO_BUFFER,
+                                   in_port = ofproto.OFPP_CONTROLLER,
+                                   actions=actions, data=data)
+        datapath.send_msg(out)
+
+    def handle_ip(self, dpid, proto, sPort=None, dPort=None, lsp_id):
+        if (sPort is None):
+            cookie = random.randint(1,65535)
+        else:
+            cookie = sPort * 65536 + dPort
+        
+        lsp_rules[lsp_id][cookie] = [dpid, proto, sPort, dPort]
+
+        if (dpid == 82):  # in from west
+            # handle west
+            datapath = datapaths[82]
+            match = datapath.ofproto_parser.OFPMatch(dl_type=ether_types.ETH_TYPE_IP,
+                                                     nw_proto=proto, tp_src=sPort,
+                                                     tp_dst=dPort,
+                                                     in_port=vBundle_info[2])
+            
+            nat_src = self.path_info[lsp_id][0]
+            nat_dst = self.path_info[lsp_id][1]
+            actions = [parser.OFPActionSetNwSrc(nat_src),
+                       parser.OFPActionSetDlSrc(self.arptable[nat_src]),
+                       parser.OFPActionSetNwDst(nat_dst),
+                       parser.OFPActionSetDlDst(self.arptable[nat_dst]),
+                       parser.OFPActionOutput(self.path[lsp_id][2])]
+
+            self.add_flow(datapath, match, actions, cookie)
+
+
+            # handle east
+            datapath = datapaths[81]
+            match = datapath.ofproto_parser.OFPMatch(dl_type=ether_types.ETH_TYPE_IP,
+                                                     nw_proto=proto, tp_src=sPort,
+                                                     tp_dst=dPort,
+                                                     in_port=self.path_info[lsp_id][2])
+
+            nat_src = self.vBundle_info[0]
+            nat_dst = self.vBundle_info[1]
+            actions = [parser.OFPActionSetNwSrc(nat_src),
+                       parser.OFPActionSetDlSrc(self.arptable[nat_src]),
+                       parser.OFPActionSetNwDst(nat_dst),
+                       parser.OFPActionSetDlDst(self.arptable[nat_dst]),
+                       parser.OFPActionOutput(self.vBundle_info[2])]
+            
+            self.add_flow(datapath, match, actions, cookie)
+
+        elif (dpid == 81):   # in from east
+            # handle east
+            datapah = datapaths[81]
+            match = datapath.ofproto_parser.OFPMatch(dl_type=ether_types.ETH_TYPE_IP,
+                                                     nw_proto=proto, tp_src=sPort,
+                                                     tp_dst=dPort,
+                                                     in_port=vBundle_info[2])
+
+            nat_src = self.path_info[lsp_id][1]
+            nat_dst = self.path_info[lsp_id][0]
+            actions = [parser.OFPActionSetNwSrc(nat_src),
+                       parser.OFPActionSetDlSrc(self.arptable[nat_src]),
+                       parser.OFPActionSetNwDst(nat_dst),
+                       parser.OFPActionSetDlDst(self.arptable[nat_dst]),
+                       parser.OFPActionOutput(self.path[lsp_id][2])]
+
+            self.add_flow(datapath, match, actions, cookie)
+
+            # handle east
+            datapath = datapaths[82]
+            match = datapath.ofproto_parser.OFPMatch(dl_type=ether_types.ETH_TYPE_IP,
+                                                     nw_proto=proto, tp_src=sPort,
+                                                     tp_dst=dPort,
+                                                     in_port=self.path_info[lsp_id][2])
+
+            nat_src = self.vBundle_info[0]
+            nat_dst = self.vBundle_info[1]
+            actions = [parser.OFPActionSetNwSrc(nat_src),
+                       parser.OFPActionSetDlSrc(self.arptable[nat_src]),
+                       parser.OFPActionSetNwDst(nat_dst),
+                       parser.OFPActionSetDlDst(self.arptable[nat_dst]),
+                       parser.OFPActionOutput(self.vBundle_info[2])]
+
+            self.add_flow(datapath, match, actions, cookie)
+            
+
+    def add_flow(self, datapath, match, actions, cookie=0, priority=100):
         ofproto = datapath.ofproto
         mod = datapath.ofproto_parser.OFPFlowMod(
             datapath=datapath, match=match, cookie=0,
             command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
-            priority=ofproto.OFP_DEFAULT_PRIORITY,
-            flags=ofproto.OFPFF_SEND_FLOW_REM, actions=actions)
+            priority=100,
+            flags=ofproto.OFPFF_SEND_FLOW_REM, actions=actions, cookie=cookie)
         datapath.send_msg(mod)
 
-    def add_flow_path(self, proto, wPort, ePort, lsp_id):
-        # handle west
+    def del_flow(self, cookie):
+        datapath = datapaths[81]
+        ofproto = datapath.ofproto
+        mod = datapath.ofproto_parser.OFPFlowMod(
+            datapath=datapath, cookie=cookie,
+            command=ofproto.OFPFC_DELETE, out_port=ofproto.OFPP_ANY,
+            out_group=ofproto.OFPG_ANY)
         datapath = datapaths[82]
-        match = datapath.ofproto_parser.OFPMatch(dl_type=ether_types.ETH_TYPE_IP,
-                                                 proto, tp_src=wPort,
-                                                 tp_dst=ePort, in_port=2)
-        actions = [parser.OFPActionSetNwSrc(self.flowpath[lsp_id]),
-                   parser.OFPActionSetDlSrc(self.arptable["192.168.5.1"]),
-                   parser.OFPActionSetNwDst("192.168.5.2"),
-                   parser.OFPActionSetDlDst(self.arptable["192.168.5.2"]),
-                   parser.OFPActionOutput(2)]
-        self.add_flow(datapath, match, actions)
-        # handle east
 
+    def packet_out(datapath, data, in_port, out_port):
+        actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
+        out = datapath.ofproto_parser.OFPPacketOut(
+            datapath=datapath, buffer_id=datapath.ofproto.OFP_NO_BUFFER,
+            in_port=in_port, actions, data=data)
+        datapath.send_msg(out)
+
+"""  Obsolete
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         msg = ev.msg
@@ -62,7 +253,7 @@ class SimpleSwitch(app_manager.RyuApp):
         # match = datapath.ofproto_parser.OFPMatch(dl_type=ether_types.ETH_TYPE_ARP)
         match = datapath.ofproto_parser.OFPMatch()
         # default fwd everything
-        self.add_flow(datapath, match, actions)
+        self.add_flow(datapath, match, actions, 0)
 
         if dpid == 82 or dpid == 81:
             self.datapaths[dpid] = dapapath
@@ -109,57 +300,4 @@ class SimpleSwitch(app_manager.RyuApp):
                        parser.OFPActionSetDlDst(self.arptable["192.168.5.1"]),
                        parser.OFPActionOutput(2)]
             self.add_flow(datapath, match, actions)
-
-    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def _packet_in_handler(self, ev):
-        msg = ev.msg
-        datapath = msg.datapath
-        ofproto = datapath.ofproto
-
-        pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocol(ethernet.ethernet)
-        pkt_arp = pkt.get_protocol(arp.arp)
-        in_port = msg.in_port
-
-        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            # ignore lldp packet
-            return
-
-        if eth.ethertype == ether_types.ETH_TYPE_ARP:
-            self._handle_arp(datapath, in_port, eth, pkt_arp)
-
-    def _handle_arp(self, datapath, port, eth, pkt_arp):
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-
-        if pkt_arp.opcode != arp.ARP_REQUEST:
-            return
-        pkt = packet.Packet()
-
-        if pkt_arp.dst_ip in self.arptable:
-           fin_src_mac = self.arptable[pkt_arp.dst_ip]
-
-        pkt.add_protocol(ethernet.ethernet(ethertype=eth.ethertype, dst=eth.src,
-                                           src=fin_src_mac))
-        pkt.add_protocol(arp.arp(opcode=arp.ARP_REPLY,
-                                 src_mac=fin_src_mac, src_ip=pkt_arp.dst_ip,
-                                 dst_mac=eth.src, dst_ip=pkt_arp.src_ip))
-        pkt.serialize()
-        data = pkt.data
-        actions = [parser.OFPActionOutput(port=port)]
-        self.logger.info("packet out for arp reply %s" %(pkt,))
-
-        out = parser.OFPPacketOut( datapath=datapath,
-                                   buffer_id=ofproto.OFP_NO_BUFFER,
-                                   in_port = ofproto.OFPP_CONTROLLER,
-                                   actions=actions, data=data)
-        datapath.send_msg(out)
-
-    def add_flow(self, datapath, match, actions):
-        ofproto = datapath.ofproto
-        mod = datapath.ofproto_parser.OFPFlowMod(
-            datapath=datapath, match=match, cookie=0,
-            command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
-            priority=ofproto.OFP_DEFAULT_PRIORITY,
-            flags=ofproto.OFPFF_SEND_FLOW_REM, actions=actions)
-        datapath.send_msg(mod)
+"""
