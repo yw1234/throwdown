678f805d13c4187a248b8d49a84e6afdeb853cd3
Bo Yan <boven.yan@gmail.com>
2016-02-26 01:34:34 -0500

    db added

--- switching/ports_east.sh
+++ switching/ports_east.sh
@@ -2,7 +2,7 @@ ovs-vsctl add-br vBundle
 # ovs-vsctl add-port vBundle eth1
 # ovs-vsctl add-port vBundle eth2
 ovs-vsctl add-port vBundle eth3
-# ovs-vsctl add-port vBundle eth4
 ovs-vsctl add-port vBundle eth5 -- set interface eth5 type=internal
+ovs-vsctl add-port vBundle eth4
 ovs-vsctl set interface eth mac=\"00:00:00:00:00:51\"
 arp -s 192.168.5.2 00:00:00:00:00:51
